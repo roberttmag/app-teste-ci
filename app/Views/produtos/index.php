2510c9aa53f3b6b0c7f16434a453b14c229c355c
Roberttmag <145805081+roberttmag@users.noreply.github.com>
2025-09-19 16:32:07 -0300

    analitycs

--- app/Views/produtos/index.php
+++ app/Views/produtos/index.php
@@ -5,6 +5,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Produtos</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-F2SHY9VDLL"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-F2SHY9VDLL');
+</script>
+
 </head>
 <body class="bg-light">
     <div class="container py-5">
